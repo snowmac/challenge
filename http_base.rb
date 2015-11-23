926881f962f27737375a758d4a1c69f64994a18e
Adam Bourg <Adam.Bourg@gmail.com>
2015-11-23 14:32:23 -0700

    Adding the basic config and http class

---
+++ http_base.rb
@@ -0,0 +1,50 @@
+require 'net/http'
+
+# Use this class to talk to the APIs
+class HttpBase
+
+  def initialize
+    @config = YAML::load_file(File.join(__dir__, 'config/apis.yml'))
+  end
+
+  def run(options)
+    method = options[:method]
+    url = options[:url]
+    data = options[:data]
+
+    uri = URL("#{@config.api_base_url}/#{url}")
+
+    if method.downcase == 'get'
+      run_get(uri)
+    elsif method.downcase == 'post'
+      run_post(uri,data)
+    end
+  end
+
+  private 
+
+  def run_get(uri)
+    req = Net::HTTP::Get.new(uri)
+    req['Auth-Token'] = @config.key
+
+    res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+      http.request(req)
+    }
+
+    res.body
+  end
+
+  def run_post(uri, data)
+    req = Net::HTTP::Post.new(uri)
+    req['Auth-Token'] = @config.key
+    req.content_type = @config.content_type
+    req.body = data
+
+    res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+      http.request(req)
+    }
+    
+    res.body
+  end
+
+end
